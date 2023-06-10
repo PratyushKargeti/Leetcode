5046c29cedf965075f8ae68c35451280deda752c
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-11 01:23:53 +0530

    Time: 0 ms (100%), Space: 6 MB (53.38%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,10 +6,10 @@ public:
         {
             long long dig=x%10;
             rev=rev*10+dig;
+            if(rev>INT_MAX || rev<INT_MIN)
+                return 0;
             x/=10;
         }
-        if(rev>INT_MAX || rev<INT_MIN)
-            return 0;
         return rev;
     }
 };
