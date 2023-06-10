a84d89f95d576607675faa407a9b1dbbafcc37e0
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-11 00:53:58 +0530

    Time: 24 ms (51.34%), Space: 8.2 MB (83.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        //abca //abcababcdc
         unordered_map<char,int>m;
-        int r=0,l=0,ans=0;
-        for(r=0;r<s.size();r++)
+        int l=0,r=0,res=0;
+        for(int r=0;r<s.size();r++)
         {
-            if(m[s[r]]>0) //if s[r] already exist in map
-                l=max(l,m[s[r]]);
-            m[s[r]]=r+1; // update current s[r]'s value
-            ans=max(ans,m[s[r]]-l);  //m[s[r]]-l is current substring length
+            char curr=s[r];
+            if(m[curr]>0) //if s[r] already exist in map
+                l=max(l,m[curr]);
+            m[curr]=r+1;  // update current s[r]'s value
+            res=max(res,m[curr]-l);  //m[s[r]]-l is current substring
         }
-        return ans;
+        return res;
     }
 };
