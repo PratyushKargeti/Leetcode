4dd5f23c437af3f6ccf3e255bdf7031ec466d1d9
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-12 22:40:42 +0530

    Time: 0 ms (100%), Space: 9.8 MB (19.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        //low variable represents the lower bound of the search range, while high represents the upper bound. The binary search algorithm continually updates the values of low and high to narrow down the search range until the target element is found or the search range becomes empty.
         int low=0;
         int high=nums.size()-1;
         while(low<=high)
@@ -15,6 +16,6 @@ public:
             else
                 high=mid-1;
         }
-        return low;
+        return low;// represents the index where the target element should be inserted to maintain the sorted order of the vector.
     }
 };
