63ae8b879d647050eb8ce9ee5c20dbf75c3ba1ba
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-13 18:54:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.59%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,12 +3,12 @@ public:
     int solve(int i,int j,vector<vector<int>>&dp,vector<vector<int>>&ob,int m,int n)
     {
         if(i==m-1 && j==n-1 && ob[i][j]!=1)
-            return 1;
+            return 1;   // Reached the bottom-right corner without obstacles
         if(i>m-1 || j> n-1 || ob[i][j]==1)
-            return 0;
+            return 0;  // Out of bounds or obstacle encountered
         if(dp[i][j]!=-1)
             return dp[i][j];
-        return dp[i][j]=solve(i+1,j,dp,ob,m,n) + solve(i,j+1,dp,ob,m,n);
+        return dp[i][j]= solve(i+1,j,dp,ob,m,n) + solve(i,j+1,dp,ob,m,n);  // Return precomputed result
     }
     int uniquePathsWithObstacles(vector<vector<int>>& ob) {
         int m=ob.size();
