08250fc5de98e8897ccc58fc4c5f494521b09deb
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-13 23:01:04 +0530

    Time: 0 ms (100%), Space: 5.9 MB (95.73%) - LeetHub

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        // int x = (a | b) ^ c;
+        // return __builtin_popcount(x) + __builtin_popcount(a & b & x);
+        int ans=0;
+        while(a!=0 | b!=0 | c!=0)
+        {
+            int x=a&1;
+            int y=b&1;
+            int z=c&1;
+            if(z==1)
+            {
+                if(x==0 && y==0)
+                    ans++;
+            }
+            else if(z==0)
+            {
+                ans+= x+y; //lsb of both a and b must be 0 
+            }
+            a>>=1;
+            b>>=1;
+            c>>=1;
+        }
+        return ans;
+    }
+};
