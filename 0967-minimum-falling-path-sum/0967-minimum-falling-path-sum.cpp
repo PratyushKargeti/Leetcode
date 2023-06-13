b098ca47503bc31cd8c9b962d33190050d26e75e
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-13 21:07:01 +0530

    Time: 24 ms (10.84%), Space: 10.7 MB (6.7%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,27 +1,27 @@
-class Solution {
-public:
-    int solve(int i,int j,vector<vector<int>>&mat, vector<vector<int>>&dp,int m,int n)
-    {
-        if(i == m-1 && j < n && j >= 0) 
-            return mat[i][j]; 
-        if(j > n-1 || j < 0) 
-            return INT_MAX;
-        if(dp[i][j]!=INT_MAX)
-            return dp[i][j];
-        return dp[i][j]=mat[i][j]+min({solve(i+1,j-1,mat,dp,m,n),solve(i+1,j,mat,dp,m,n),solve(i+1,j+1,mat,dp,m,n)});
-    }
-    int minFallingPathSum(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
-        int sum=INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            sum=min(sum,solve(0,i,mat,dp,m,n));
-        }
-        return sum;
-    }
-};
+// class Solution {
+// public:
+//     int solve(int i,int j,vector<vector<int>>&mat, vector<vector<int>>&dp,int m,int n)
+//     {
+//         if(i == m-1 && j < n && j >= 0) 
+//             return mat[i][j]; 
+//         if(j > n-1 || j < 0) 
+//             return INT_MAX;
+//         if(dp[i][j]!=INT_MAX)
+//             return dp[i][j];
+//         return dp[i][j]=mat[i][j]+min({solve(i+1,j-1,mat,dp,m,n),solve(i+1,j,mat,dp,m,n),solve(i+1,j+1,mat,dp,m,n)});
+//     }
+//     int minFallingPathSum(vector<vector<int>>& mat) {
+//         int m=mat.size();
+//         int n=mat[0].size();
+//         vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
+//         int sum=INT_MAX;
+//         for(int i=0;i<n;i++)
+//         {
+//             sum=min(sum,solve(0,i,mat,dp,m,n));
+//         }
+//         return sum;
+//     }
+// };
  //Bottom-up Recursive with memo
 // class Solution {
 // public:
@@ -43,3 +43,32 @@ public:
 //         return ans;
 //     }
 // };
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>&mat, vector<vector<int>>&dp,int m,int n)
+    {
+        if(i>m-1 || j>n-1 || i<0 || j<0)
+        {
+            return INT_MAX;
+        }
+        if(i==m-1 || (i==m-1 && j==n-1))
+        {
+            return mat[i][j];
+        }
+        if(dp[i][j]!=INT_MAX)
+            return dp[i][j];
+        return dp[i][j]=mat[i][j]+min({solve(i+1,j-1,mat,dp,m,n),solve(i+1,j,mat,dp,m,n),solve(i+1,j+1,mat,dp,m,n)});
+    }
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
+        int sum=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            int x=solve(0,i,mat,dp,m,n);
+            sum=min(sum,x);
+        }
+        return sum;
+    }
+};
