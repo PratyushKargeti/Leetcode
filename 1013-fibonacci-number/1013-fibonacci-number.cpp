dc2a0ce21b5724789c9087a020b3637863af13a7
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-16 12:49:48 +0530

    Time: 0 ms (100%), Space: 6.1 MB (25.71%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int lookupfn(int n,vector<int>&lookup)
-    {
-        if(lookup[n]<INT_MAX)
-            return lookup[n];
-        else
-        {
-            if(n<=1)
-                lookup[n]=n;
-            else
-                lookup[n]=lookupfn(n-1,lookup)+lookupfn(n-2,lookup);
-        }
-        return lookup[n];
-    }
+//     int lookupfn(int n,vector<int>&lookup)
+//     {
+//         if(lookup[n]<INT_MAX)
+//             return lookup[n];
+//         else
+//         {
+//             if(n<=1)
+//                 lookup[n]=n;
+//             else
+//                 lookup[n]=lookupfn(n-1,lookup)+lookupfn(n-2,lookup);
+//         }
+//         return lookup[n];
+//     }
 
+//     int fib(int n) {
+// //Using memoization
+//         vector<int> lookup(n+1,INT_MAX);
+//         return lookupfn(n,lookup);
+//     }
     int fib(int n) {
-//Using memoization
-        vector<int> lookup(n+1,INT_MAX);
-        return lookupfn(n,lookup);
+        vector<int>dp(n+2);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
