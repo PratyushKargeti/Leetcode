a752a06df5fd0c25695bd858d535702a93bf4302
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-21 22:46:24 +0530

    Time: 139 ms (59.85%), Space: 67.5 MB (6%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -11,39 +11,39 @@
  */
 class Solution {
 public:
-    // vector<int>v;
-    // void inorder(TreeNode* root)
-    // {
-    //     if(root==NULL)
-    //         return;
-    //     inorder(root->left);
-    //     v.push_back(root->val);
-    //     inorder(root->right);
-    // }
-    // int rangeSumBST(TreeNode* root, int low, int high) {
-    //     inorder(root);
-    //     int sum=0;
-    //     for(int i=0;i<v.size();i++)
-    //     {
-    //         if(v[i]>=low && v[i]<=high)
-    //             sum+=v[i];    
-    //     }
-    //     return sum;
-    // }
+    vector<int>v;
+    void inorder(TreeNode* root)
+    {
+        if(root==NULL)
+            return;
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-    if (root == nullptr) {
-        return 0;
+        inorder(root);
+        int sum=0;
+        for(int i=0;i<v.size();i++)
+        {
+            if(v[i]>=low && v[i]<=high)
+                sum+=v[i];    
+        }
+        return sum;
     }
+//     int rangeSumBST(TreeNode* root, int low, int high) {
+//     if (root == nullptr) {
+//         return 0;
+//     }
     
-    if (root->val < low) {
-        return rangeSumBST(root->right, low, high);
-    }
+//     if (root->val < low) {
+//         return rangeSumBST(root->right, low, high);
+//     }
     
-    if (root->val > high) {
-        return rangeSumBST(root->left, low, high);
-    }
+//     if (root->val > high) {
+//         return rangeSumBST(root->left, low, high);
+//     }
     
-    return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
-}
+//     return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+// }
 
 };
