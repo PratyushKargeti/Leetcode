2df1384b882772d0771597f62185eb1bf64561e3
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-24 13:07:59 +0530

    Time: 22 ms (61.42%), Space: 13.2 MB (33.64%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,38 +1,53 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) { 
-        int count=0;
-        vector<vector<int>>v;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=0;j<nums.size();j++)
+//      //   brute force
+//         int count=0;
+//         vector<vector<int>>v;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             for(int j=0;j<nums.size();j++)
+//             {
+//                 if(i!=j)
+//                 {
+//                     if(abs(nums[i]-nums[j])==k)
+//                     {
+//                         count++;   
+//                         v.push_back({min(nums[i],nums[j]),max(nums[i],nums[j])});
+//                     }
+//                 }
+//             }
+//         }   
+//         //// for(int i=0;i<v.size();i++)
+//         // {
+//         //     for(int j=0;j<v[0].size();j++)
+//         //         cout<<v[i][j]<<" ";
+//         //     cout<<endl;
+//         // }//
+//         sort(v.begin(),v.end());
+//       //  // cout<<"------\n";//
+//         v.erase(unique(v.begin(),v.end()),v.end());
+// //// for(int i=0;i<v.size();i++)
+//         // {
+//         //     for(int j=0;j<v[0].size();j++)
+//         //         cout<<v[i][j]<<" ";
+//         //     cout<<endl;
+//       //  // }
+//         return v.size();
+
+            unordered_map<int,int>m;
+            int ans=0;
+            for(int i=0;i<nums.size();i++)
             {
-                if(i!=j)
-                {
-                    if(abs(nums[i]-nums[j])==k)
-                    {
-                        count++;   
-                        v.push_back({min(nums[i],nums[j]),max(nums[i],nums[j])});
-                    }
-                }
+                m[nums[i]]++;
             }
-        }   
-        // for(int i=0;i<v.size();i++)
-        // {
-        //     for(int j=0;j<v[0].size();j++)
-        //         cout<<v[i][j]<<" ";
-        //     cout<<endl;
-        // }
-        sort(v.begin(),v.end());
-        // cout<<"------\n";
-        v.erase(unique(v.begin(),v.end()),v.end());
-        // for(int i=0;i<v.size();i++)
-        // {
-        //     for(int j=0;j<v[0].size();j++)
-        //         cout<<v[i][j]<<" ";
-        //     cout<<endl;
-        // }
-        return v.size();
-        //return count;
+            for(auto i:m)
+            {
+                if(k>0 && m.count(i.first+k)>0)
+                    ans++;
+                else if(k==0 && i.second>1)
+                    ans++;
+            }
+            return ans;
     }
 };
