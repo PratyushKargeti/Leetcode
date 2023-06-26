df118479637356c3641911c32b8bebdd5f585d2c
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-27 01:27:48 +0530

    Time: 5 ms (37.59%), Space: 5.9 MB (81.78%) - LeetHub

--- 2284-smallest-value-of-the-rearranged-number/2284-smallest-value-of-the-rearranged-number.cpp
+++ 2284-smallest-value-of-the-rearranged-number/2284-smallest-value-of-the-rearranged-number.cpp
@@ -21,7 +21,7 @@ public:
             }
         }
         str+=to_string(mini);
-        cout<<str<<endl;
+        //cout<<str<<endl;
         int j;
         for(int i=0;i<s.size();i++)
         {
@@ -32,58 +32,13 @@ public:
                 break;
             }
         }
-        cout<<j<<endl;
+        //cout<<j<<endl;
         s.erase(j,1);
         sort(s.begin(),s.end(),[&](char &a,char &b){
-            //if(a!=mini-'0' && b!=mini-'0')
                 return a<b;
-            
         });
         str+=s;
-        cout<<str;
+        //cout<<str;
         return stoll(str);
-        // vector<long long>v;
-        // long long n=num;
-        // if(num<0)
-        //     n=-num;
-        // while(n)
-        // {
-        //     v.push_back(n%10);
-        //     n/=10;
-        // }
-        // if(num<0)
-        // {
-        //     sort(v.begin(),v.end(),greater<int>());
-        //     string ans="";
-        //     for(int i=0;i<v.size();i++)
-        //     {
-        //         ans+=to_string(v[i]);
-        //     }
-        //     cout<<ans;
-        //     long long nnn=stoll(ans);
-        //     return -nnn;
-        // }
-        // long long m;
-        // if(v.size()==1)
-        //     return v[0];
-        // sort(v.begin(),v.end());
-        // string ans="";
-        // if(v[0]==0)
-        // {    
-        //     ans+=to_string(v[1]);
-        //     for(int i=0;i<v.size();i++)
-        //     {
-        //         if(i!=1)
-        //             ans+=to_string(v[i]);
-        //     }
-        // }
-        // else
-        // {
-        //     for(int i=0;i<v.size();i++)
-        //         ans+=to_string(v[i]);
-        // }
-        // cout<<ans;
-        // m=stoll(ans);
-        // return m;
     }
 };
