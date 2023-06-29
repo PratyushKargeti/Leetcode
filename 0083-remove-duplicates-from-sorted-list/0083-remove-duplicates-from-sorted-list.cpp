5d826d32233d1eb9ddff3c6bc71aa02b2fac0541
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-06-29 15:11:10 +0530

    Time: 10 ms (70.38%), Space: 11.5 MB (96.99%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -10,44 +10,60 @@
  */
 class Solution {
 public:
-    ListNode* vtoll(vector<int>& ans)
-    {
-        ListNode* head=nullptr;
-        ListNode* tail=nullptr;
-        for(int i=0;i<ans.size();i++)
-        {
-            ListNode* newnode=new ListNode;
-            newnode->val=ans[i];
-            newnode->next=nullptr;
-            if(head==nullptr)
-            {
-                head=newnode;
-                tail=newnode;
-            }
-            else
-            {
-                tail->next =newnode;
-                tail=newnode;
-            }
-        }
-        return head;
-    }
+    // ListNode* vtoll(vector<int>& ans)
+    // {
+    //     ListNode* head=nullptr;
+    //     ListNode* tail=nullptr;
+    //     for(int i=0;i<ans.size();i++)
+    //     {
+    //         ListNode* newnode=new ListNode;
+    //         newnode->val=ans[i];
+    //         newnode->next=nullptr;
+    //         if(head==nullptr)
+    //         {
+    //             head=newnode;
+    //             tail=newnode;
+    //         }
+    //         else
+    //         {
+    //             tail->next =newnode;
+    //             tail=newnode;
+    //         }
+    //     }
+    //     return head;
+    // }
+    // ListNode* deleteDuplicates(ListNode* head) {
+    //     vector<int>v;
+    //     ListNode* ptr=head;
+    //     while(ptr!=nullptr)
+    //     {
+    //         v.push_back(ptr->val);
+    //         ptr=ptr->next;
+    //     }
+    //     set<int>S;
+    //     S.insert(v.begin(),v.end());
+    //     vector<int>ans;
+    //     for(auto it=S.begin();it!=S.end();it++)
+    //     {
+    //         ans.push_back(*it);
+    //     }
+    //     ListNode *start=vtoll(ans);
+    //     return start;
+    // }
     ListNode* deleteDuplicates(ListNode* head) {
-        vector<int>v;
-        ListNode* ptr=head;
-        while(ptr!=nullptr)
+        if(head == NULL) return head;
+        ListNode*ptr1=head;
+        ListNode*ptr2=head->next;
+        while(ptr2)
         {
-            v.push_back(ptr->val);
-            ptr=ptr->next;
+            if(ptr1->val!=ptr2->val)
+            {   
+                ptr1->next=ptr2;
+                ptr1=ptr1->next;
+            }
+            ptr2=ptr2->next;
         }
-        set<int>S;
-        S.insert(v.begin(),v.end());
-        vector<int>ans;
-        for(auto it=S.begin();it!=S.end();it++)
-        {
-            ans.push_back(*it);
-        }
-        ListNode *start=vtoll(ans);
-        return start;
+        ptr1->next=NULL;
+        return head;
     }
 };
