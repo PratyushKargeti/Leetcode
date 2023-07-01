61e1f25e54fbb719715bbb91797e5bad1572d57c
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-01 09:54:43 +0530

    Time: 78 ms (79.05%), Space: 6.9 MB (62.54%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int solve(vector<int>&cookies,vector<int>&sums,int k,int ind,int zerocount)
     {
-        if(cookies.size()-ind<zerocount)
-            return INT_MAX;
+        //zerocount stores no. of children with no cookie
+        if(zerocount > cookies.size()-ind) 
+            return INT_MAX; //invalid distribution
         if(ind==cookies.size())
-        {
-            return *max_element(sums.begin(),sums.end());
-        }
-
+            return *max_element(sums.begin(),sums.end()); //fair answer return
         int res=INT_MAX;
-        for(int i=0;i<k;i++)
-        {
-            if(sums[i]==0)
-                zerocount--;
-            sums[i]+=cookies[ind];
+        for(int i=0;i<k;i++) 
+        { //ith child
+            if(sums[i]==0) //if sum of cookies for this child is zero
+                zerocount--; //we will give this child cookie
+            sums[i]+=cookies[ind]; 
             res=min(res,solve(cookies,sums,k,ind+1,zerocount));
-            sums[i]-=cookies[ind];
-            if(sums[i]==0)
-                zerocount++;
+            sums[i]-=cookies[ind]; //backtrack 
+            if(sums[i]==0) // if sum again becomes zero after backtrack
+                zerocount++; //no of child with zero cookie increased again
         }
         return res;
     }
