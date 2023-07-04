791ff582d40d42e6321329c7792654505b21db63
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-05 01:36:26 +0530

    Time: 4 ms (80.11%), Space: 6.1 MB (78.84%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int ans;
-    bool canPlace(int currRow, vector<int>& a) {
-        for(int row = 0; row < currRow; row++)
-            if(a[currRow] == a[row] || abs(a[currRow]-a[row]) == currRow-row) 
-                return false;
-        return true;
-    }
-    
-    void dfs(int i, int n, vector<int>& a, vector<string>& v) {
-        if(i == n){
-            ans++;
+    void solve(int col,int &count, vector<int>&leftrow,vector<int>&lowerdiagonal,vector<int>&upperdiagonal,int n)
+    {
+        if(col==n)
+        {
+            count++;
             return;
         }
-        for(int col = 0; col < n; col++)
+        for(int row=0;row<n;row++)
         {
-            a[i] = col;
-            if(canPlace(i, a))
+            if(!leftrow[row]&&!lowerdiagonal[row+col]&&!upperdiagonal[n-1+col-row])
             {
-                v[i][col] = 'Q';
-                dfs(i+1, n, a, v);
-                v[i][col] = '.';
+                leftrow[row]=1;
+                lowerdiagonal[row+col]=1;
+                upperdiagonal[n-1+col-row]=1;
+                solve(col+1,count,leftrow,lowerdiagonal,upperdiagonal,n);
+                leftrow[row]=0;
+                lowerdiagonal[row+col]=0;
+                upperdiagonal[n-1+col-row]=0;
             }
         }
     }
     int totalNQueens(int n) {
-        vector<int> a(n);
-        vector<string> v(n);        
-        for(int i = 0; i < n; i++) 
-            v[i] = string(n, '.');
-        
-        dfs(0, n, a, v);
-        return ans;
+        vector<int>leftrow(n,0);
+        vector<int>lowerdiagonal(2*n-1,0);
+        vector<int>upperdiagonal(2*n-1,0);
+        int count=0;
+        solve(0,count,leftrow,lowerdiagonal,upperdiagonal,n);
+        return count;
     }
 };
