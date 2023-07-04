2baab9d86e102542c2e6ab1f793bf200d7d0e20a
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-04 19:59:31 +0530

    Time: 132 ms (79.23%), Space: 91.5 MB (99.65%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
-        auto it1=max_element(nums.begin(),nums.end());
-        auto it2=min_element(nums.begin(),nums.end());
-        int maxe=*it1;
-        int mine=*it2;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=maxe && nums[i]!=mine)
-                return nums[i];
-        }
-        return -1;
+    //     auto it1=max_element(nums.begin(),nums.end());
+    //     auto it2=min_element(nums.begin(),nums.end());
+    //     int maxe=*it1;
+    //     int mine=*it2;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         if(nums[i]!=maxe && nums[i]!=mine)
+    //             return nums[i];
+    //     }
+    //     return -1;
+    // }
+
+        sort(nums.begin(), nums.end());
+        return nums.size() > 2 ? nums[1] : - 1;
     }
+
 };
