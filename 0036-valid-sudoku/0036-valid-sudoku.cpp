c59a499a2eaa148e4ebf948eccf31d3a1eae83f0
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-05 03:10:57 +0530

    Time: 25 ms (55.87%), Space: 17.9 MB (92.24%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-bool issafe(int row, int col, vector<vector<char>>& board, char ch) {
-    for (int i = 0; i < 9; i++) {
-        if (i != col && board[row][i] == ch) // same row check
-            return false;
-        if (i != row && board[i][col] == ch) // same column check
-            return false;
-        
-        int startRow = 3 * (row / 3) + i / 3;
-        int startCol = 3 * (col / 3) + i % 3;
-        if ((startRow != row || startCol != col) && board[startRow][startCol] == ch) // same block check
-            return false;
+    bool issafe(int row, int col, vector<vector<char>>& board, char ch) {
+        for (int i = 0; i < 9; i++) 
+        {
+            if(i != col && board[row][i] == ch) // same row check 
+                return false;
+            if (i != row && board[i][col] == ch) // same column check
+                return false;
+            int startRow = 3 * (row / 3) + i / 3;
+            int startCol = 3 * (col / 3) + i % 3;
+            if((startRow != row || startCol != col) && board[startRow][startCol] == ch) // same block check
+                return false;
+        }
+        return true;
     }
-    return true;
-}
-
     bool isValidSudoku(vector<vector<char>>& board) {
         int n=board.size();
         for(int i=0;i<n;i++)
