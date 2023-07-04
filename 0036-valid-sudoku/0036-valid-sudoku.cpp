4e02864e305a2ab966b4742f66f5766b299b008d
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-05 03:08:22 +0530

    Time: 16 ms (93.9%), Space: 17.9 MB (92.24%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-bool issafe(int row, int col, vector<vector<char>>& board) {
-    char ch = board[row][col]; // value in the current cell
-
-    // Check same row
+bool issafe(int row, int col, vector<vector<char>>& board, char ch) {
     for (int i = 0; i < 9; i++) {
-        if (i != col && board[row][i] == ch)
+        if (i != col && board[row][i] == ch) // same row check
+            return false;
+        if (i != row && board[i][col] == ch) // same column check
+            return false;
+        
+        int startRow = 3 * (row / 3) + i / 3;
+        int startCol = 3 * (col / 3) + i % 3;
+        if ((startRow != row || startCol != col) && board[startRow][startCol] == ch) // same block check
             return false;
     }
-
-    // Check same column
-    for (int i = 0; i < 9; i++) {
-        if (i != row && board[i][col] == ch)
-            return false;
-    }
-
-    // Check same block
-    int startRow = 3 * (row / 3);
-    int startCol = 3 * (col / 3);
-    for (int i = startRow; i < startRow + 3; i++) {
-        for (int j = startCol; j < startCol + 3; j++) {
-            if (i != row && j != col && board[i][j] == ch)
-                return false;
-        }
-    }
-
     return true;
 }
+
     bool isValidSudoku(vector<vector<char>>& board) {
         int n=board.size();
-    for(int i=0;i<n;i++){
-       for(int j=0;j<n;j++){
-           if(board[i][j]!='.'){
-               if(issafe(i,j,board)==false)
-               return false;
-           }
-       }
-    }
-return true;
-   
-    }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                char ch=board[i][j];
+                if(ch!='.')
+                {
+                    if(issafe(i,j,board,ch)==false)
+                        return false;
+                }
+            }
+        }
+        return true;
+   }
 };
