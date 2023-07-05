74d8aa990ca3ce4008c564017e985d338b7bf321
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-05 12:25:45 +0530

    Time: 45 ms (59.3%), Space: 36.4 MB (98.32%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int zerocount=0;  // Number of zero's in the window.
-        int l=0;
-        int start=0;// Left end of the window.
-        for(int i=0;i<nums.size();i++)
+        // int zerocount=0;  // Number of zero's in the window.
+        // int l=0;
+        // int start=0;// Left end of the window.
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]==0)
+        //         zerocount++;
+        //     // Shrink the window until the zero counts come under the limit.
+        //     while(zerocount>1)
+        //     {
+        //         if(nums[start]==0)
+        //             zerocount--;
+        //         start++;
+        //     }
+        //     l=max(l,i-start); 
+        // }
+        // return l;
+        int start=0,i;
+        int k=1;
+        for(i=0;i<nums.size();i++)
         {
             if(nums[i]==0)
-                zerocount++;
-            // Shrink the window until the zero counts come under the limit.
-            while(zerocount>1)
-            {
-                if(nums[start]==0)
-                    zerocount--;
-                start++;
-            }
-            l=max(l,i-start); 
+                k--;
+            if(k<0 && nums[start++]==0)
+                k++;
         }
-        return l;
+        return i-start-1;
     }
 };
