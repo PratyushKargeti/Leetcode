478b453d56ffc4909ec0f34c8b27aeddf23b2c6d
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-06 12:07:35 +0530

    Time: 6 ms (22.88%), Space: 7.1 MB (50.11%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,11 +5,25 @@ public:
         //reverse matrix rows
         reverse(mat.begin(),mat.end());
         
+        //7 8 9
+        //4 5 6
+        //1 2 3
+        
         //transpose the matrix
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<=i;j++)
+            for(int j=i;j<n;j++)
                 swap(mat[i][j],mat[j][i]);
         }
+
+        //i=0 
+        // mat[0][0] /mat[0][0]
+        // mat[0][1] /mat[1][0]
+        // mat[0][2] /mat[2][0]
+        //i=1
+        // mat[1][1] /mat[1][1]
+        // mat[1][2] /mat[2][1]
+        //i=2
+        // mat[2][2] /mat[2][2]
     }
 };
