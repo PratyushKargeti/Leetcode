ad43ce2b017acd1fdb81a57e4ef2fc5021865fd0
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-07 13:34:23 +0530

    Time: 12 ms (77.46%), Space: 10.3 MB (54.8%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
+        //any O which is connected to boundary O is useless(no conversion)
+        //all remaining O's which are within boundary are useful(convertible)
         queue<pair<int,int>>q;
         int m=board.size();
         int n=board[0].size();
@@ -11,7 +13,7 @@ public:
             {
                 if((i==0 || i==m-1 || j==0 || j==n-1) && board[i][j]=='O')
                 {
-                    q.push({i,j});
+                    q.push({i,j}); //push all boundary O's in queue
                     visited[i][j]=true;
                 }
             }
@@ -28,7 +30,7 @@ public:
                 int newj=frontj+moves[i][1];
                 if(newi>=0 && newi<m && newj>=0 && newj<n && visited[newi][newj]==false && board[newi][newj]=='O')
                 {
-                    q.push({newi,newj}); //useless 'O' which wil not be converted into 'X' will be pushed in queue
+                    q.push({newi,newj}); //useless 'O' which will not be converted into 'X' will be pushed in queue
                     visited[newi][newj]=true;
                 }
             }
@@ -38,7 +40,7 @@ public:
             for(int j=0;j<n;j++)
             {
                 if(board[i][j]=='O' && visited[i][j]==false)
-                    board[i][j]='X'; //useful unvisited O's must be surrounded all by X
+                    board[i][j]='X'; //useful unvisited O's must be surrounded all by X so convertible
             }
         }
     }
