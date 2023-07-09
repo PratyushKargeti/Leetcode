afec95993e6f940e8a8dfd3df874444c58c49f5a
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-09 13:07:47 +0530

    Time: 126 ms (50.51%), Space: 67.6 MB (83.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,9 +5,9 @@ public:
         int max_sum=nums[0];
         for(int i=1;i<nums.size();i++)
         {
-            curr_sum=max(nums[i],curr_sum+nums[i]);
-            if(curr_sum > max_sum)
-                max_sum=curr_sum;
+            curr_sum=max(nums[i],curr_sum+nums[i]); //update current sum
+            if(curr_sum > max_sum) 
+                max_sum=curr_sum; //update maxsum
         }
         return max_sum;
     }
