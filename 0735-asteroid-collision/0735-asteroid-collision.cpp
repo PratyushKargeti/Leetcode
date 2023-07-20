7e68217ef96bf0143703ce8ae2a568cdfc5f3759
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-07-20 13:06:47 +0530

    Time: 12 ms (83.98%), Space: 17.3 MB (99.00%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -7,6 +7,9 @@ public:
 		    while(res.size() && res.back() > 0 && a < 0 && !destroyed) {
 			    if(abs(res.back()) >= abs(a)) destroyed = true;
 			    if(abs(res.back()) <= abs(a)) res.pop_back();
+                //NOTE HERE TWO IFS ARE BACK TO BACK FOR HANDING abs(res.back())==abs(a) case
+                //then current asteroid 'a' will be destroyed(will note be added) and last
+                //element of res will also be destroyed,i.e, both will be destroyed
 		    }
 		    if(destroyed==false) res.push_back(a);
 	    }
