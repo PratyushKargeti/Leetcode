36919fc82994df3713821e3d24da6ebe34e185ff
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-04 22:56:01 +0530

    Time: 423 ms (12.99%), Space: 456 MB (5.95%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -14,23 +14,54 @@ public:
     }
     int countSubstrings(string s) {
         int n=s.length();
-        vector<string>substrings;
+        int count=0;
         for(int i=0;i<n;i++) 
         {
             for(int j=i;j<n;j++) 
             {
-                substrings.push_back(s.substr(i,j-i+1));
+                string sub=s.substr(i,j-i+1);
+                if(palin(sub))
+                    count++;
             }
-        }
-        int count=0;
-        for(string str:substrings) 
-        {
-            if(palin(str))
-                count++;
         }
         return count;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool palin(string& s){ 
+//         if(s.size()==0) return false;
+//         int i=0;
+//         int j=s.size()-1;
+//         while(i<=j)
+//         {
+//             if(s[i]!=s[j]) return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+//     int countSubstrings(string s) {
+//         int n=s.length();
+//         vector<string>substrings;
+//         for(int i=0;i<n;i++) 
+//         {
+//             for(int j=i;j<n;j++) 
+//             {
+//                 substrings.push_back(s.substr(i,j-i+1));
+//             }
+//         }
+//         int count=0;
+//         for(string str:substrings) 
+//         {
+//             if(palin(str))
+//                 count++;
+//         }
+//         return count;
+//     }
+// };
 
 
 
