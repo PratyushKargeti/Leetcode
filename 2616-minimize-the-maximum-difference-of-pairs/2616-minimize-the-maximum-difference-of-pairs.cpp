a968a525394d13b2bb56bcf35570f661acd8abcf
PratyushKargeti <135763048+PratyushKargeti@users.noreply.github.com>
2023-08-09 08:53:50 +0530

    Time: 130 ms (94.27%), Space: 81.1 MB (38.62%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-//Count how many pairs in nums have a difference equal to or less than mid
-    int countpairs(int mid,vector<int>&nums)
+//Check if count of pairs in nums having a difference equal to or less than threshold mid(possible answer) is greater than equal to p
+    //greedy provides larger subarray for finding the next pair
+    bool count_verifypairs(int mid,vector<int>&nums,int p)
     {
         int count=0;
         for(int i=0;i<nums.size()-1;)
@@ -13,24 +14,25 @@ public:
             }
             i++;
         }
-        return count;
+        return count>=p;
 
     }
     int minimizeMax(vector<int>& nums, int p) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); //greedy approach will always yield the maximum number of valid pairs.
         int n=nums.size();
         int low=0;
         int high=nums[n-1]-nums[0];
-        while(low<high)
+        while(low<high) //at low>=high, we will have our answer stored in row
         {
             int mid=low+(high-low)/2;
-            if(countpairs(mid,nums)>=p)
+            //given threshold mid, we determine if there exist at least p valid pairs?
+            if(count_verifypairs(mid,nums,p))
             {
-                high=mid;
+                high=mid; //look for more less answer in left part 
             }
             else
             {
-                low=mid+1;
+                low=mid+1; //look for more greatwer answer in right part
             }
         }
         return low;
